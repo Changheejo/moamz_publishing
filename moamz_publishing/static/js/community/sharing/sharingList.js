a679c7183f0317400539ae95952138d5cd004faf
PANGJINI-D <geniedev97@gmail.com>
2024-10-12 21:24:07 +0900

    241012 21:23 황진 나눔게시판 목록 완료(페이지네이션 아직 안함)

---
+++ moamz_publishing/static/js/community/sharing/sharingList.js
@@ -0,0 +1,35 @@
+const availableBtn = document.getElementById('sharing-available-btn');
+const reservedBtn = document.getElementById('sharing-reserved-btn');
+const completedBtn = document.getElementById('sharing-completed-btn');    
+const availableList = document.getElementById('sharing-available-list');
+const reservedList = document.getElementById('sharing-reserved-list');
+const completedList = document.getElementById('sharing-completed-list');
+
+availableBtn.addEventListener('click', () => {
+    availableBtn.classList.add('active');
+    reservedBtn.classList.remove('active');
+    completedBtn.classList.remove('active');
+    availableList.classList.remove('hidden');
+    reservedList.classList.add('hidden');
+    completedList.classList.add('hidden');
+});
+
+reservedBtn.addEventListener('click', () => {
+    availableBtn.classList.remove('active');
+    reservedBtn.classList.add('active');
+    completedBtn.classList.remove('active');
+    availableList.classList.add('hidden');
+    reservedList.classList.remove('hidden');
+    completedList.classList.add('hidden');
+});
+
+completedBtn.addEventListener('click', () => {
+    availableBtn.classList.remove('active');
+    reservedBtn.classList.remove('active');
+    completedBtn.classList.add('active');
+    availableList.classList.add('hidden');
+    reservedList.classList.add('hidden');
+    completedList.classList.remove('hidden');
+});
+
+// 페이지네이션..
