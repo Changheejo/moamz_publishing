87c9ad6d14f35e097d8e882387cef50430470e66
liahkimi <maruanna1994@gmail.com>
2024-10-16 15:52:15 +0900

    241016 15:51 김가은 admin 네비게이션바 js 수정

--- moamz_publishing/static/js/admin/adminUserInquiryDetail.js
+++ moamz_publishing/static/js/admin/adminUserInquiryDetail.js
@@ -3,3 +3,10 @@ const editBtn = document.getElementById('edit-btn');
 const deleteBtn= document.getElementById('delete-btn');
 
 
+submitBtn.addEventListener('click', ()=>{
+    const confirmMsg = confirm('댓글을 등록하시겠습니까?');
+    if(confirmMsg){
+        // ?
+    }
+})
+
